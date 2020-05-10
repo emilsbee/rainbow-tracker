21f4e58023c6dbbd83b80b28c37c8a3853f4abbc
Emils Bernhards <emilsbernhards@Emilss-MacBook-Pro.local>
2020-05-10 20:04:52 +0200

    Updates

---
+++ precache-manifest.7e76f3460384b00b00c4b067516cdb53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d9364f088cae77349277e774a78230",
+    "url": "/rainbow-tracker/index.html"
+  },
+  {
+    "revision": "97aac5e5dcb978d6f3dc",
+    "url": "/rainbow-tracker/static/css/main.08d2e9df.chunk.css"
+  },
+  {
+    "revision": "d3006c483e6041e2baf7",
+    "url": "/rainbow-tracker/static/js/2.985ae717.chunk.js"
+  },
+  {
+    "revision": "bb6c30348a2416dcf4621a34e1a9218d",
+    "url": "/rainbow-tracker/static/js/2.985ae717.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97aac5e5dcb978d6f3dc",
+    "url": "/rainbow-tracker/static/js/main.034872d5.chunk.js"
+  },
+  {
+    "revision": "0a96e60a8c2769170b12",
+    "url": "/rainbow-tracker/static/js/runtime-main.6526d97a.js"
+  }
+]);
