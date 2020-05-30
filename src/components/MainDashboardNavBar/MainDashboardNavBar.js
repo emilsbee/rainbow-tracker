a4864e7e602936d8285a24f0067bc237602d9f5a
Emils Bernhards <emilsbernhards@Emilss-MacBook-Pro.local>
2020-05-30 18:46:24 +0200

    Added return to current week button

--- src/components/MainDashboardNavBar/MainDashboardNavBar.js
+++ src/components/MainDashboardNavBar/MainDashboardNavBar.js
@@ -1,5 +1,6 @@
 // External imports
 import React, { useState, useEffect } from 'react'
+import moment from 'moment'
 import { useStoreActions } from 'easy-peasy'
 
 
@@ -16,6 +17,16 @@ const MainDashboardNavBar = ({ weekNr, year, years, weeks, weekid }) => {
     const stopWeekListener = useStoreActions(actions => actions.weeks.stopWeekListener)
     
     
+    const[isCurrentWeek, setIsCurrentWeek] = useState(true)
+
+    useEffect(() => {
+        if (weekNr !== moment().week() || year !== moment().year()) {
+                setIsCurrentWeek(false)
+        } else {
+            setIsCurrentWeek(true)
+        }
+    }, [year, weekNr])
+
     const handleYearDropdown = (e) => {
             stopWeekListener({weekid})
             startWeekListener({type:'SPECIFIC_WEEK', year: e, weekNr: 1, weekid})
@@ -39,14 +50,26 @@ const MainDashboardNavBar = ({ weekNr, year, years, weeks, weekid }) => {
         startWeekListener({type: 'NEXT_WEEK', weekNr, year, weekid})
     }
 
+    const handleCurrentWeek = () => {
+        startWeekListener({type: 'CURRENT_WEEK', init: true})
+    }
+
     return (
         <div className="container">
 
-            <div className="banners">
+            <div className="banners" style={{"justifyContent": !isCurrentWeek ? 'space-between' : 'flex-end'}}>
+            {!isCurrentWeek && <button 
+                className="to-current-week"
+                    onClick={handleCurrentWeek}
+                >
+                    To current week
+                </button>}
+                <div className="banners-nav" style={{"marginRight": isCurrentWeek && '-60px'}}>
                 <BackArrow onClick={handlePrevWeek} className="previous-week" />
                     <h2 className="year-banner">{year},</h2>
                     <h2 className="week-banner">week {weekNr}</h2>
                 <NextArrow onClick={handleNextWeek} className="next-week" />
+                </div>
             </div>
             <div className="navigation">
                 
