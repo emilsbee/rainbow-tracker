4783419db713f6a386bc67db60476f9a8a638372
Emils Bernhards <emilsbernhards2001@gmail.com>
2020-11-21 17:35:08 +0100

    Containerized the app. Now implementing docker-compose with separate container for setting up ssl renewal

--- src/components/firebase/firebase.js
+++ src/components/firebase/firebase.js
@@ -14,8 +14,8 @@ var firebaseConfig = {
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
     measurementId: process.env.REACT_APP_MEASUREMENT_ID
-
 };
+
 
 firebase.initializeApp(firebaseConfig)
 
