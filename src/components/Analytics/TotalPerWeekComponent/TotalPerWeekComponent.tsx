d7c7b47881f7f1b3c2450ad16edc98ab68cb0db2
Emils Bernhards <bernhardsemils@gmail.com>
2021-08-17 15:06:44 +0200

    Starting new branch to implemented specific date for categories and notes.

--- src/components/Analytics/TotalPerWeekComponent/TotalPerWeekComponent.tsx
+++ src/components/Analytics/TotalPerWeekComponent/TotalPerWeekComponent.tsx
@@ -13,28 +13,43 @@ const TotalPerWeekComponent = ({totalPerWeek}: TotalPerWeekProps) => {
 
     return (
         <table>
-            {totalPerWeek.categoryTypes.map(categoryType => (
-                <>
-                    {totalPerWeek.activityTypes.map((activityType, index) => {
+            <tbody>
+            {totalPerWeek.categoryTypes.map(categoryType => {
+                console.log(categoryType)
+                if (!totalPerWeek.activityTypes.find(el => el.categoryid === categoryType.categoryid)) { // If the current category doesn't have any activities
+                    return (
+                        <tr key={categoryType.categoryid}>
+                            <td className={"total-per-week-table__category-row__cell cell"}>{categoryType.name}</td>
+                            <td className={"total-per-week-table__category-row__cell-count cell"}>{categoryType.count}</td>
+                        </tr>
+                    )
+                } else { // If the current category does have activities
 
-                        if (index === 0) {
-                            return (
-                                <tr>
-                                    <td className={"total-per-week-table__category-row__cell cell"}>{categoryType.name}</td>
-                                    <td className={"total-per-week-table__category-row__cell-count cell"}>{categoryType.count}</td>
-                                </tr>
-                            )
-                        } else if (activityType.categoryid === categoryType.categoryid) {
-                            return (
-                                <tr>
-                                    <td className={"total-per-week-table__activity-row__cell cell"}>{activityType.long}</td>
-                                    <td className={"total-per-week-table__activity-row__cell-count cell"}>{activityType.count}</td>
-                                </tr>
-                            )
-                        }
-                    })}
-                </>
-            ))}
+                    return (
+                        <>
+                            {totalPerWeek.activityTypes.map((activityType, index) => {
+                                console.log(activityType)
+                                if (index === 0) { // Render the category before all its activities
+                                    return (
+                                        <tr key={categoryType.categoryid}>
+                                            <td className={"total-per-week-table__category-row__cell cell"}>{categoryType.name}</td>
+                                            <td className={"total-per-week-table__category-row__cell-count cell"}>{categoryType.count}</td>
+                                        </tr>
+                                    )
+                                } else if (activityType.categoryid === categoryType.categoryid) {
+                                    return (
+                                        <tr key={activityType.activityid}>
+                                            <td className={"total-per-week-table__activity-row__cell cell"}>{activityType.long}</td>
+                                            <td className={"total-per-week-table__activity-row__cell-count cell"}>{activityType.count}</td>
+                                        </tr>
+                                    )
+                                }
+                            })}
+                        </>
+                    )
+                }
+            })}
+            </tbody>
         </table>
     )
 }
